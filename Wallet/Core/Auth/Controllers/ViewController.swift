3811fc22fc0f5378a29bf0ab9c4c7fdf16d2b1bb
Daria Kokareva <kokareva.daria@gmail.com>
2019-09-22 22:02:16 +0200

    initial commit, proj + gitignore

---
+++ Wallet/Core/Auth/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wallet
+//
+//  Created by Daria Kokareva on 01/09/2019.
+//  Copyright © 2019 Enecuum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

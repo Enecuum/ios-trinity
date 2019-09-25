0328b6eec988f113fae1ecc989b2a7a6863300dc
Daria Kokareva <kokareva.daria@gmail.com>
2019-09-25 03:38:19 +0200

    added swap view stub

--- Wallet/Core/Main/Controllers/TransferViewController.swift
+++ Wallet/Core/Main/Controllers/TransferViewController.swift
@@ -9,6 +9,8 @@ class TransferViewController: UIViewController {
 
     @IBOutlet weak var tabsView: TabsView!
     @IBOutlet weak var sendView: SendView!
+    @IBOutlet weak var receiveView: ReceiveView!
+    @IBOutlet weak var swapView: SwapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +19,30 @@ class TransferViewController: UIViewController {
         tabsView.selectorViewColor = .white
         tabsView.selectorTextColor = .white
         tabsView.textColor = .white
+        tabsView.delegate = self
+
+        sendView.isHidden = false
+        receiveView.isHidden = true
+        swapView.isHidden = true
+    }
+}
+
+extension TransferViewController: CustomSegmentedControlDelegate {
+    func changeToIndex(index: Int) {
+        switch index {
+        case 0:
+            sendView.isHidden = false
+            receiveView.isHidden = true
+            swapView.isHidden = true
+        case 1:
+            sendView.isHidden = true
+            receiveView.isHidden = false
+            swapView.isHidden = true
+        case 2:
+            sendView.isHidden = true
+            receiveView.isHidden = true
+            swapView.isHidden = false
+        default: ()
+        }
     }
 }

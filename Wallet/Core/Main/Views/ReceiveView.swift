cbe5f6aa7f7e97125682cac214210de373cc74b6
Daria Kokareva <kokareva.daria@gmail.com>
2019-09-27 09:39:56 +0200

    updated receive view

--- Wallet/Core/Main/Views/ReceiveView.swift
+++ Wallet/Core/Main/Views/ReceiveView.swift
@@ -7,19 +7,30 @@ import UIKit
 
 class ReceiveView: UIView, NibView {
 
+    @IBOutlet weak var addressTextField: UITextField!
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadViewFromNib()
+        setup()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         loadViewFromNib()
+        setup()
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
         roundCorners(corners: [.topRight, .topLeft], radius: 25)
+    }
+
+    private func setup() {
+        addressTextField.text = CryptoHelper.getAddress()
+    }
+    
+    @IBAction func onCopyAddressClicked(_ sender: Any) {
+        UIPasteboard.general.string = addressTextField.text
     }
 }

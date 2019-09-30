04a1041c7b33ace9449ef0446adc76eebe37e5e1
Daria Kokareva <kokareva.daria@gmail.com>
2019-09-30 05:07:02 +0200

    tuned slider a bit

--- Wallet/Core/Main/Views/Slider.swift
+++ Wallet/Core/Main/Views/Slider.swift
@@ -7,10 +7,26 @@ import UIKit
 
 class Slider: UISlider {
 
-    @IBInspectable open var trackWidth: CGFloat = 2 {
+    @IBInspectable open var trackWidth: CGFloat = 40 {
         didSet {
             setNeedsDisplay()
         }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        setThumbImage(R.image.transfer.thumb()!, for: .normal)
+        setThumbImage(R.image.transfer.thumb()!, for: .highlighted)
+
+        var leftTrackImage = R.image.transfer.minTrack()!
+       // let insets: UIEdgeInsets = UIEdgeInsets(top: 20, left: 40, bottom: 20, right: 40)
+       // leftTrackImage = leftTrackImage.resizableImage(withCapInsets: insets)
+        setMinimumTrackImage(leftTrackImage, for: .normal)
+
+        var rightTrackImage = R.image.transfer.maxTrack()!
+       // rightTrackImage = rightTrackImage.resizableImage(withCapInsets: insets)
+        setMaximumTrackImage(rightTrackImage, for: .normal)
     }
 
     override open func trackRect(forBounds bounds: CGRect) -> CGRect {

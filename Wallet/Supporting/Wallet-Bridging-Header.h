8e1d3362af1ccd39abf94a9ced8f86d71ec9c18c
Daria Kokareva <kokareva.daria@gmail.com>
2019-09-30 22:15:50 +0200

    added bridging header

---
+++ Wallet/Supporting/Wallet-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Wallet-Bridging-Header.h
+//  Wallet
+//
+//  Created by Daria Kokareva on 30/09/2019.
+//  Copyright © 2019 Enecuum. All rights reserved.
+//
+
+#ifndef Wallet_Bridging_Header_h
+#define Wallet_Bridging_Header_h
+
+
+#endif /* Wallet_Bridging_Header_h */

a8446ab1510131fb2a3fdc86378a1a772e91c86f
Daria Kokareva <kokareva.daria@gmail.com>
2019-10-16 03:33:19 +0300

    added main tabs logic only: container controller + tabs, screens transition

--- Wallet/Core/Main/Controllers/MainViewController.swift
+++ Wallet/Core/Main/Controllers/MainViewController.swift
@@ -7,16 +7,57 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var mainTabsView: TabsView!
+
+    private enum State: Int {
+        case stats = 0
+        case transition
+    }
+
+    private var state: State?
+    
+    private var currentViewController: UIViewController?
     private var transferViewController: TransferViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         Defaults.setRunOnceFlag()
+
+        mainTabsView.setButtonTitles(buttonTitles: [R.string.localizable.send.localized(), R.string.localizable.receive.localized()])
+        mainTabsView.selectorViewColor = .white
+        mainTabsView.selectorTextColor = .white
+        mainTabsView.textColor = .white
+        mainTabsView.delegate = self
+
+        if state == nil {
+            transit(to: .stats)
+        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let transferViewController = segue.destination as? TransferViewController {
             self.transferViewController = transferViewController
+        }
+    }
+
+    // MARK: - Transitions
+
+    private func transit(to state: State) {
+        if let vc = viewController(for: state) {
+            currentViewController?.removeFromParentVc()
+            addChildVc(vc, in: containerView)
+            currentViewController = vc
+            self.state = state
+        }
+    }
+
+    private func viewController(for state: State) -> UIViewController? {
+        switch state {
+        case .stats:
+            return R.storyboard.main.statisticsViewController()
+        case .transition:
+            return R.storyboard.main.transferViewController()
         }
     }
 
@@ -30,6 +71,14 @@ class MainViewController: UIViewController {
     @IBAction func unwindToMainViewController(_ segue: UIStoryboardSegue) {
         if let _ = segue.source as? PrivateKeyViewController {
             transferViewController?.resetBalance()
+        }
+    }
+}
+
+extension MainViewController: CustomSegmentedControlDelegate {
+    func changeToIndex(index: Int) {
+        if let state = State(rawValue: index) {
+            transit(to: state)
         }
     }
 }
---
+++ Wallet/Core/Main/Controllers/StatisticsViewController.swift
@@ -0,0 +1,30 @@
+//
+// Created by Daria Kokareva on 24/09/2019.
+// Copyright (c) 2019 Enecuum. All rights reserved.
+//
+
+import UIKit
+import QRCodeReader
+import AVFoundation
+
+class StatisticsViewController: UIViewController {
+
+    struct Constants {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+    }
+
+    // MARK: - Public methods
+
+
+    // MARK: - Server
+
+}

15c6fadcfe6116969842ddcc38fb75f4cd78675f
Daria Kokareva <kokareva.daria@gmail.com>
2019-11-26 19:10:45 -0500

    updated gradient button drawing

--- Wallet/Extensions/GradientButton.swift
+++ Wallet/Extensions/GradientButton.swift
@@ -7,6 +7,12 @@ import UIKit
 
 class GradientButton: UIButton {
 
+    override var bounds: CGRect {
+        didSet {
+            updateGradient()
+        }
+    }
+
     private var gradientLayer: CAGradientLayer?
 
     @IBInspectable private var firstColor: UIColor = UIColor.clear {

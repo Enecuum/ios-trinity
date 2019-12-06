204ae456e6e01e8e4f788cbf30965913e4132066
Daria Kokareva <kokareva.daria@gmail.com>
2019-12-05 23:43:18 -0500

    added touch on stake slider

--- Wallet/Core/Main/Views/Slider.swift
+++ Wallet/Core/Main/Views/Slider.swift
@@ -20,13 +20,12 @@ class Slider: UISlider {
         setThumbImage(R.image.transfer.thumb()!, for: .highlighted)
 
         let leftTrackImage = R.image.transfer.minTrack()!
-       // let insets: UIEdgeInsets = UIEdgeInsets(top: 20, left: 40, bottom: 20, right: 40)
-       // leftTrackImage = leftTrackImage.resizableImage(withCapInsets: insets)
         setMinimumTrackImage(leftTrackImage, for: .normal)
 
         let rightTrackImage = R.image.transfer.maxTrack()!
-       // rightTrackImage = rightTrackImage.resizableImage(withCapInsets: insets)
         setMaximumTrackImage(rightTrackImage, for: .normal)
+
+        addTapGesture()
     }
 
     override open func trackRect(forBounds bounds: CGRect) -> CGRect {
@@ -39,4 +38,19 @@ class Slider: UISlider {
                 height: trackWidth
         )
     }
+
+    public func addTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        addGestureRecognizer(tap)
+    }
+
+    @objc private func handleTap(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: self)
+        let percent = minimumValue + Float(location.x / bounds.width) * maximumValue
+        setValue(percent, animated: true)
+        sendActions(for: .valueChanged)
+    }
+    /*override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        true
+    }*/
 }

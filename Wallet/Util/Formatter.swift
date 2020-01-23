ef90ba33e55f60c68cb68398fb020e65f733404b
Daria Kokareva <kokareva.daria@gmail.com>
2020-01-22 21:24:10 -0500

    updated amount formatter

--- Wallet/Util/Formatter.swift
+++ Wallet/Util/Formatter.swift
@@ -7,7 +7,7 @@ import Foundation
 
 class Formatter {
 
-    static func decimalString(_ decimal: NSDecimalNumber, fractionDigits: Int = 6, decimalSeparator: String = ".") -> String {
+    static func decimalString(_ decimal: NSDecimalNumber, fractionDigits: Int = 10, decimalSeparator: String = ".") -> String {
         let formatter = NumberFormatter()
         formatter.maximumFractionDigits = fractionDigits
         formatter.decimalSeparator = decimalSeparator
